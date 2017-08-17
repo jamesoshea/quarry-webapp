47e34aee0e1474ee875bffcc7369a3294ecb1fc6
James O'Shea <jamesoshea89@hotmail.com>
2017-08-17 13:53:11 +0200

    export functions added

--- src/components/left-components/DisplayScrape.vue
+++ src/components/left-components/DisplayScrape.vue
@@ -1,8 +1,13 @@
 <template lang="html">
-  <div>
+  <div class="q-scrape-main">
     <div v-if="currentScrape">
-      <h5>{{new Date(currentScrape.timeStamp).toLocaleString()}}</h5>
+      <h5 class="centre">{{new Date(currentScrape.timeStamp).toLocaleString()}}</h5>
       <p>{{currentScrape.url}}</p>
+      <p class="centre">Export as:
+        <input class="form-input centre" v-model="filename" placeholder="Filename">
+        <span class="q-fake-link" @click="exportFile(currentScrape.rows, 'csv')">CSV</span> |
+        <span class="q-fake-link" @click="exportFile(currentScrape.rows, 'tsv')">TSV</span>
+      </p>
       <table class="table table-striped table-hover q-table">
         <thead>
           <tr>
@@ -26,6 +31,7 @@
 export default {
   data() {
     return {
+      filename: ''
     }
   },
   computed: {
@@ -37,10 +43,73 @@ export default {
     }
   },
   methods: {
+    exportFile(rows, format) {
+      let processRow = function (row) {
+        let finalVal = ''
+        for (let j = 0; j < row.length; j++) {
+          let innerValue = row[j] === null ? '' : row[j].toString()
+          if (row[j] instanceof Date) {
+            innerValue = row[j].toLocaleString()
+          }
+          let result = innerValue.replace(/"/g, '""')
+          if (result.search(/("|,|\n)/g) >= 0) {
+            result = '"' + result + '"'
+          }
+          if (j > 0) {
+            if (format === 'csv') {
+              finalVal += ','
+            } else if (format === 'tsv') {
+              finalVal += '\t'
+            }
+          }
+          finalVal += result
+        }
+        return finalVal + '\n'
+      };
 
+      let csvFile = ''
+      for (let i = 0; i < rows.length; i++) {
+        csvFile += processRow(rows[i])
+      }
+      let filename = this.filename
+      if (format === 'csv') {
+        filename += '.csv'
+      } else if (format === 'tsv') {
+        filename += '.tsv'
+      }
+      const blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' })
+      if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename)
+      } else {
+        let link = document.createElement("a")
+        if (link.download !== undefined) { // feature detection
+          // Browsers that support HTML5 download attribute
+          let url = URL.createObjectURL(blob)
+          link.setAttribute("href", url)
+          link.setAttribute("download", filename)
+          link.style.visibility = 'hidden'
+          document.body.appendChild(link)
+          link.click()
+          document.body.removeChild(link)
+        }
+      }
+    }
   }
 }
 </script>
 
-<style lang="css">
+<style>
+
+.q-scrape-main {
+  margin-top: 1.5rem;
+}
+
+.centre {
+  text-align: center;
+}
+
+.q-fake-link {
+  cursor: pointer;
+}
+
 </style>
